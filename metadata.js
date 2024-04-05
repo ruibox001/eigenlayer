8d784e78e2877ab3cb3ae829ae6691bc22454813
ruds <wangshengyuancrazy@163.com>
2024-04-05 20:34:11 +0800

    Create metadata.js

---
+++ metadata.js
@@ -0,0 +1,7 @@
+{
+  "name": "androidTools",
+  "website": "https://twitter.com/androidTools",
+  "description": "developer android apps",
+  "logo": "https://raw.githubusercontent.com/sy4484/eigenlayer/main/web3tools.png",
+  "twitter": "https://twitter.com/androidTools"
+}
