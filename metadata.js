c5c536a2e7c138992e0681998cf143ea58967b6a
ruds <wangshengyuancrazy@163.com>
2024-04-19 21:34:58 +0800

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -2,6 +2,6 @@
   "name": "EigenlayerTool",
   "website": "https://github.com/ruibox001",
   "description": "developer eigenlayer apps",
-  "logo": "https://raw.githubusercontent.com/ruibox001/eigenlayer/main/androidtools.png",
+  "logo": "https://raw.githubusercontent.com/ruibox001/eigenlayer/main/androidtool.png",
   "twitter": "https://twitter.com/EigenlayerTool"
 }
